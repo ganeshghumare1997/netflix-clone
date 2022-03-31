34ee115770e7fd3fa87aca219a88cb498cca2ba9
Ganesh Ghumare <ganeshghumare1997@gmail.com>
2022-03-31 12:53:15 +0530

    Created basic setup and added fonts

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Head, Html, NextScript, Main } from "next/document";
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
