6b03cc9966c8e96c3b03e0cbab998357d3940582
Ganesh Ghumare <ganeshghumare1997@gmail.com>
2022-04-02 12:42:46 +0530

    Added Youtube Videos Library and Homepage done.

---
+++ components/card/section-cards.js
@@ -0,0 +1,21 @@
+import Card from "./card";
+import styles from "./section-cards.module.css";
+
+const SectionCards = (props) => {
+    const { title, videos=[], size } = props;
+
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.cardWrapper}>
+        {
+            videos.map((video, index)=>(
+                <Card id={index} key={index} imgUrl={video.imgUrl} size={size} />
+            ))
+        }
+      </div>
+    </section>
+  );
+};
+
+export default SectionCards;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,25 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
-import Card from "../components/card/card";
+import { getVideos, getPopularVideos } from "../lib/videos";
+import SectionCards from "../components/card/section-cards";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("Productivity");
+  const travelVideos = await getVideos("Travel");
+  const popularVideos = await getPopularVideos();
+  return {
+    props: { disneyVideos, productivityVideos, travelVideos, popularVideos },
+  };
+}
+
+export default function Home({
+  disneyVideos,
+  productivityVideos,
+  travelVideos,
+  popularVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,16 +29,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <NavBar username="Ganesh"/>
-      <Banner
-        title="Clifford the red dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/clifford.webp"
-      />
-
-      <Card imgUrl="/static/clifford.webp" size="large"/>
-      <Card imgUrl="/static/clifford.webp" size="medium"/>
-      <Card imgUrl="/static/clifford.webp" size="small"/>
+      <div className={styles.main}>
+        <NavBar username="Ganesh" />
+        <Banner
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
+        />
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          <SectionCards title="Travel" videos={travelVideos} size="medium" />
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+          <SectionCards title="Popular" videos={popularVideos} size="small" />
+        </div>
+      </div>
     </div>
   );
 }
